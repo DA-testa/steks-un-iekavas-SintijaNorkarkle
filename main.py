be31aa1f64ba764b76e15d9729b27561d7c0258b
Sintija Norkārkle <124626861+SintijaNorkarkle@users.noreply.github.com>
2023-02-19 12:16:00 +0000

    -

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def find_mismatch(text):
                 # pārbauda, vai steks ir tukšs, vai arī aizverošā iekava nesakrīt ar pēdējo atverošo iekavu stekā
                 return i+1 # atgriež pašreizējo indeksu "i" +1, tādā veidā norādot pirmās nesakrītošās iekavas pozīciju
             
-                opening_brackets_stack.pop() # tiek noņemta pēdējā atverošā iekava no steka, jo tā sakrīt ar pašreizējo aizverošo iekavu
+            opening_brackets_stack.pop() # tiek noņemta pēdējā atverošā iekava no steka, jo tā sakrīt ar pašreizējo aizverošo iekavu
         
     if opening_brackets_stack: # pārbauda, vai steks nav tukšs
         return opening_brackets_stack[0][1] #atgriež pirmās nesakrītošās atverošās iekavas pozīciju stekā
