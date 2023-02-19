45a37dda57bc25d40bc3bd9e68bde7cd581af4f2
Sintija Norkārkle <124626861+SintijaNorkarkle@users.noreply.github.com>
2023-02-19 12:02:00 +0000

    -

--- main.py
+++ main.py
@@ -13,16 +13,19 @@ def find_mismatch(text):
     opening_brackets_stack = []
     for i, next in enumerate(text):
         if next in "([{":  
-            opening_brackets_stack.append((next, i)) # tiek pievienota atverošā iekava un attiecīgais kārtas numurs
+            opening_brackets_stack.append(Bracket(next, 1+i)) # tiek pievienota atverošā iekava un attiecīgais kārtas numurs
             
 
         if next in ")]}":
             if not opening_brackets_stack or not are_matching(opening_brackets_stack[-1][0], next): 
                 # pārbauda, vai steks ir tukšs, vai arī aizverošā iekava nesakrīt ar pēdējo atverošo iekavu stekā
                 return i+1 # atgriež pašreizējo indeksu "i" +1, tādā veidā norādot pirmās nesakrītošās iekavas pozīciju
+            
             opening_brackets_stack.pop() # tiek noņemta pēdējā atverošā iekava no steka, jo tā sakrīt ar pašreizējo aizverošo iekavu
+        
         if opening_brackets_stack: # pārbauda, vai steks nav tukšs
             return opening_brackets_stack[0][1] #atgriež pirmās nesakrītošās atverošās iekavas pozīciju stekā
+    
     return "Success" # izvada "Success", ja visas iekavas ir vienāda skaita ( katrai atverošajai iekavai ir attiecīgā aizverošā iekava)
             
                 
@@ -31,10 +34,11 @@ def main():
 
     text = input() #lietotājs ievada virkni, kas tiek saglabāts mainīgajā "text"
     if "I" in text: # tiek pārbaudīts, vai virknē tika ievadīts burts "I"
-        text=input() # tālāk lietotājs atkal ievada citu virkni, kas tiek saglabāts mainīgajā "text"
+        text = input() # tālāk lietotājs atkal ievada citu virkni, kas tiek saglabāts mainīgajā "text"
     
         mismatch = find_mismatch(text)
         print(mismatch) #izvada mainīgā "mismatch" vērtību
+
 
 if __name__ == "__main__":
     main()
