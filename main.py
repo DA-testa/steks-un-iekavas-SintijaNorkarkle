f30292070ef29a8e96954d8548680d98c60ce551
Sintija Norkārkle <124626861+SintijaNorkarkle@users.noreply.github.com>
2023-02-19 11:54:14 +0000

    -

--- main.py
+++ main.py
@@ -12,26 +12,29 @@ def are_matching(left, right):
 def find_mismatch(text):
     opening_brackets_stack = []
     for i, next in enumerate(text):
-        if next in "([{":
-            opening_brackets_stack.append(Bracket(next, i+1))
-            # Process opening bracket, write your code here
+        if next in "([{":  
+            opening_brackets_stack.append((next, i)) # tiek pievienota atverošā iekava un attiecīgais kārtas numurs
+            
 
         if next in ")]}":
-            # Process closing bracket, write your code here
-            if not opening_brackets_stack or not are_matching(opening_brackets_stack[-1].char, next):
-                return i+1
-            opening_brackets_stack.pop()
-            if opening_brackets_stack:
-                return opening_brackets_stack[0].position
-            return "Success"
-
-
-
+            if not opening_brackets_stack or not are_matching(opening_brackets_stack[-1][0], next): 
+                # pārbauda, vai steks ir tukšs, vai arī aizverošā iekava nesakrīt ar pēdējo atverošo iekavu stekā
+                return i+1 # atgriež pašreizējo indeksu "i" +1, tādā veidā norādot pirmās nesakrītošās iekavas pozīciju
+            opening_brackets_stack.pop() # tiek noņemta pēdējā atverošā iekava no steka, jo tā sakrīt ar pašreizējo aizverošo iekavu
+            if opening_brackets_stack: # pārbauda, vai steks nav tukšs
+                 return opening_brackets_stack[0][1] #atgriež pirmās nesakrītošās atverošās iekavas pozīciju stekā
+            elif return "Success" # izvada "Success", ja visas iekavas ir vienāda skaita ( katrai atverošajai iekavai ir attiecīgā aizverošā iekava)
+            
+                
 def main():
-    text = input()
-    mismatch = find_mismatch(text)
-    # Printing answer, write your code here
+    # I ievade jāpievieno main
 
+    text = input() #lietotājs ievada virkni, kas tiek saglabāts mainīgajā "text"
+    if "I" in text: # tiek pārbaudīts, vai virknē tika ievadīts burts "I"
+        text=input() # tālāk lietotājs atkal ievada citu virkni, kas tiek saglabāts mainīgajā "text"
+    
+        mismatch = find_mismatch(text)
+        print(mismatch) #izvada mainīgā "mismatch" vērtību
 
 if __name__ == "__main__":
     main()
